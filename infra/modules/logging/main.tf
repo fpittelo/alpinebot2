644852136248f0d1fd7cef4164ad9d5b3ca07b10
fpittelo <frederic.pitteloud@gmail.com>
2025-11-27 11:16:35 +0100

    #47 Module Logging

---
+++ infra/modules/logging/main.tf
@@ -0,0 +1,17 @@
+resource "azurerm_log_analytics_workspace" "main" {
+  name                = "${var.environment}-log-${var.project_name}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "PerGB2018"
+  retention_in_days   = var.log_retention_days
+  tags                = merge(var.tags, { Environment = var.environment })
+}
+
+resource "azurerm_application_insights" "main" {
+  name                = "${var.environment}-appi-${var.project_name}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  workspace_id        = azurerm_log_analytics_workspace.main.id
+  application_type    = "web"
+  tags                = merge(var.tags, { Environment = var.environment })
+}

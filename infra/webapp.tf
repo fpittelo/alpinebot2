4e11ec63e9b5519fad9e91dbe9de74a5e18f7174
fpittelo <frederic.pitteloud@gmail.com>
2025-11-27 11:51:02 +0100

    #47 Module Web App

--- infra/webapp.tf
+++ infra/webapp.tf
@@ -1,20 +1,15 @@
-resource "azurerm_linux_web_app" "frontend" {
-  name                = "${var.environment}-app-${var.project_name}"
-  resource_group_name = azurerm_resource_group.rg.name
+module "web_app" {
+  source = "./modules/web_app"
+
+  project_name        = var.project_name
+  environment         = var.environment
   location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.main.id
+  tags                = var.tags
+}
 
-  site_config {
-    application_stack {
-      node_version = "22-lts"
-    }
-    app_command_line = "node /home/site/wwwroot/server.js"
-  }
-
-  app_settings = {
-    "WEBSITES_PORT" = "3000"
-    "WEBSITES_CONTAINER_START_TIME_LIMIT" = "1800"
-  }
-
-  tags = merge(var.tags, { Environment = var.environment })
+moved {
+  from = azurerm_linux_web_app.frontend
+  to   = module.web_app.azurerm_linux_web_app.frontend
 }
